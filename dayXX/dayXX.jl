0cd80f84a491152956fa47cc306792226490192f
Dillon Scott <dillonscott.2024@gmail.com>
2024-11-30 01:56:56 -0500

    boilerplate

---
+++ dayXX/dayXX.jl
@@ -0,0 +1,45 @@
+using Test
+
+function part1(file::AbstractString)::AbstractString
+  s = read(file, String);
+
+  s
+end
+
+function testPart1() 
+  expected = read("./part1_test_output.txt", String);
+  actual = part1("./test_input.txt");
+
+  @test expected != actual
+  println("Test for part 1 passed!")
+end
+
+function runPart1() 
+  actual = part1("./input.txt");
+  write("./part1_output.txt", actual)
+end
+
+function part2(file::AbstractString)::AbstractString
+  s = read(file);
+
+  s
+end
+
+function testPart2() 
+  expected = read("./part2_test_output.txt", String);
+  actual = part2("./test_input.txt");
+
+  @test expected == actual
+  println("Test for part 2 passed!")
+end
+
+function runPart2() 
+  actual = part2("./input.txt");
+  write("./part2_output.txt", actual)
+end
+
+
+
+testPart1()
+
+
